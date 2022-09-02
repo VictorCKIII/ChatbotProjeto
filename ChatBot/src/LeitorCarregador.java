41f00b314de3a78aba6bf4ed0683fabf3a2da131
Victor <eduardovictor822@gmail.com>
2022-09-02 20:59:14 -0300

    Adicionando método para ler e adicionar capítulos

--- ChatBot/src/LeitorCarregador.java
+++ ChatBot/src/LeitorCarregador.java
@@ -41,4 +41,53 @@ public class LeitorCarregador {
         return personagens;
 
     }
+
+    HashMap<String, Capitulos> lerCapitulos(
+             String caminhoArquivoCapitulos,
+             HashMap<String, PersoTest> personagens,
+             Scanner escaneadorConsole)
+    {
+        HashMap<String, Capitulos> capitulos = new HashMap<String, Capitulos>();
+        File arquivoCapitulos = new File(caminhoArquivoCapitulos);
+
+        try {
+             Scanner escaneadorArquivoCapitulos = new Scanner(arquivoCapitulos, "UTF-8");
+
+             String nomeCapitulo = "";
+             String textoCapitulo = "";
+             String nomePersonagem = "";
+             int variacaoEnergia = 0;
+             String linhaEscaneada = "";
+
+             while(escaneadorArquivoCapitulos.hasNextLine())
+             {
+                 while(!linhaEscaneada.equals("CAPITULO"))
+                 {
+                    linhaEscaneada = escaneadorArquivoCapitulos.nextLine();
+                 }
+                 linhaEscaneada = escaneadorArquivoCapitulos.nextLine(); // Nome
+                 nomeCapitulo = escaneadorArquivoCapitulos.nextLine();
+                 linhaEscaneada = escaneadorArquivoCapitulos.nextLine(); // Texto
+                 textoCapitulo = escaneadorArquivoCapitulos.nextLine(); 
+                 linhaEscaneada = escaneadorArquivoCapitulos.nextLine(); // Personagem
+                 nomePersonagem = escaneadorArquivoCapitulos.nextLine(); 
+                 linhaEscaneada = escaneadorArquivoCapitulos.nextLine(); // Variação De Energia
+                 variacaoEnergia = Integer.parseInt(escaneadorArquivoCapitulos.nextLine()); 
+                 capitulos.put(nomeCapitulo, new Capitulos(nomeCapitulo, 
+                                                           textoCapitulo, 
+                                                           personagens.get(nomePersonagem), 
+                                                           variacaoEnergia,
+                                                           escaneadorConsole));
+             }
+
+             escaneadorArquivoCapitulos.close();
+        } 
+        catch (FileNotFoundException exception) 
+        {  
+            exception.printStackTrace();
+        }
+
+        return capitulos;
+
+    }
 }
