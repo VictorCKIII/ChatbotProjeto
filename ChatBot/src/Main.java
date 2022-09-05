dd9c9317ffdc70893a7b613d51032bdde7153099
Victor <eduardovictor822@gmail.com>
2022-09-05 08:48:49 -0300

    PRONTO

--- ChatBot/src/Main.java
+++ ChatBot/src/Main.java
@@ -20,45 +20,27 @@ public class Main {
                                                                          personagens,
                                                                         input);
         
-          
-
-
-            Capitulos cap1 = capitulos.get("ENCONTRO");
-
-           
-                    
-
-            Capitulos cap2 = capitulos.get("BANSHEES");
-                    
-
-            Capitulos cap3 = new Capitulos("Bruxos",
-                    "Serei bondoso com você Geralt, aqui vai uma pergunta sobre sua raça" +
-                            "Qual a rua que os bruxos mais usam na estação do inverno ?",
-                    geralt,
-                    25,
-                    input);
-
-            Capitulos cap4 = new Capitulos("VOCÊ GANHOU! PARABÉNS",
-                    "Tome aqui 100 moedas de ouro como recompensa !",
-                    geralt,
-                    100,
-                    input);
         
 
-            Capitulos cap5 = new Capitulos("RESPOSTA ERRADA !",
-                    "Então, como consequência, você irá me pagar com sua vida !",
-                    geralt,
-                    -100,
-                    input);
+
+            Capitulos raiz = capitulos.get("ENCONTRO");
+                    
+          // Capitulos cap2 = capitulos.get("BANSHEES");                  
+
+            //Capitulos cap3 = capitulos.get("BRUXOS");
+
+           // Capitulos cap4 = capitulos.get("GANHOU");
+
+          //  Capitulos cap5 = capitulos.get("PERDEU");
 
             //cap1.escolhas.add(new Escolha("Cajado Sagrado", cap2));
-            cap1.escolhas.add(new Escolha("Grimorio", cap5));
-            cap2.escolhas.add(new Escolha("Gado", cap5));
-            cap2.escolhas.add(new Escolha("Camponeses", cap3));
-            cap3.escolhas.add(new Escolha("Mana", cap5));
-            cap3.escolhas.add(new Escolha("Fogo", cap4));
+            //cap1.escolhas.add(new Escolha("Grimorio", cap5));
+            //cap2.escolhas.add(new Escolha("Gado", cap5));
+            //cap2.escolhas.add(new Escolha("Camponeses", cap3));
+            //cap3.escolhas.add(new Escolha("Mana", cap5));
+            //cap3.escolhas.add(new Escolha("Fogo", cap4));
 
-            Capitulos raiz = cap1;
+           
             raiz.mostrar();
 
             input.close();
